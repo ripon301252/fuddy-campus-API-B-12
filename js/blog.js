cd4144b65a0c452d4ccd26d1ffb972d81fff1c94
Mahfuzur  Rahman <93132951+ripon301252@users.noreply.github.com>
2025-09-20 13:53:59 +0600

    Add files via upload

---
+++ js/blog.js
@@ -0,0 +1,4 @@
+document.getElementById('btn-blog').addEventListener('click', function(){
+    window.location.href = 'index.html'
+})
+  
---
+++ js/categories_button.js
@@ -0,0 +1,26 @@
+const loadCategories = () => {
+    fetch('https://taxi-kitchen-api.vercel.app/api/v1/categories')
+        .then(res => res.json())
+        .then(api => displayCategories(api.categories))
+        .catch(err => console.log(err))
+}
+
+const displayCategories = (categories) =>{
+    // console.log(categories)
+    const categoriesButton = document.getElementById('categories-button')
+
+    for(const category of categories){
+        // console.log(category)
+        const categoryBtn = document.createElement('div');
+        categoryBtn.innerHTML = `
+            <button id = "btn-${category.id}" onclick = "loadFoods(${category.id})" class = " flex items-center hover:bg-[#e9e7e7] w-36 bg-white gap-1 mb-3 py-1 px-2 cursor-pointer rounded-full category-btn">
+                <div><img class = "w-10 h-6 mx-auto  pl-2" src="${category.categoryImg}" alt=""></div>
+                <h3 class = "text-xs text-center ">${category.categoryName}</h3>
+            </button>
+        
+        `;
+        categoriesButton.appendChild(categoryBtn)
+    }
+}
+
+loadCategories()
